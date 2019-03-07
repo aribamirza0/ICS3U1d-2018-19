a62c11094558328d466ab1168c1e27ef8ac42a42
robuntu <robuntu@stro.ycdsb.ca>
2019-03-07 10:29:13 -0500

    def functions

--- Working/Masters/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/Masters/1_Drawing_with_Functions/intro_to_functions.py
@@ -6,13 +6,24 @@ SCREEN_HEIGHT = 600
 
 def draw_cloud():
     """ draw cloud """
-    arcade.draw_circle_filled(290, 400, 60, arcade.color.WHITE)
-    arcade.draw_circle_filled(410, 400, 60, arcade.color.WHITE)
-    arcade.draw_circle_filled(350, 450, 60, arcade.color.WHITE)
-    arcade.draw_circle_filled(350, 350, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(100, 450, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(220, 450, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(160, 500, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(160, 400, 60, arcade.color.WHITE)
 
-def rolling_hills():
+def draw_rolling_hills():
     """ draw rolling hills """
+    arcade.draw_circle_filled(100, 50, 200, arcade.color.BROWN_NOSE)
+    arcade.draw_circle_filled(700, 50, 200, arcade.color.BROWN_NOSE)
+    arcade.draw_circle_filled(400, 50, 200, arcade.color.BROWN_NOSE)
+
+def draw_tree():
+    """ draw tree """
+    arcade.draw_rectangle_filled(660, 300, 100, 300, arcade.color.CHESTNUT)
+    arcade.draw_circle_filled(720, 450, 60, arcade.color.DARK_SPRING_GREEN)
+    arcade.draw_circle_filled(600, 450, 60, arcade.color.DARK_SPRING_GREEN)
+    arcade.draw_circle_filled(660, 500, 60, arcade.color.DARK_SPRING_GREEN)
+    arcade.draw_circle_filled(660, 400, 60, arcade.color.DARK_SPRING_GREEN)
 
 
 def main():
@@ -22,6 +33,9 @@ def main():
 
    # call your draw functions
     draw_cloud()
+    draw_rolling_hills()
+    draw_tree()
+
     # Finish and run
     arcade.finish_render()
     arcade.run()
