468f74098d2b7c69d93213bbabdd31b54b2aa4c6
robuntu <robuntu@stro.ycdsb.ca>
2019-03-21 11:08:32 -0400

    def functions

---
+++ Working/2_1_if_statements.py
@@ -0,0 +1,13 @@
+
+def cigar_party(cigars, is_weekend):
+    if is_weekend:
+        if cigars <= 40:
+            return True
+    else if:
+        if cigars >= 40 and cigars =< 60:
+            return True
+    else:
+    return False
+
+
+        
